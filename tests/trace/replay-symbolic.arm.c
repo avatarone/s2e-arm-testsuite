7823124de35a6d7ad49d1f86ea1d5011ca2a752c
Lucian Cojocar <cojocar@gmail.com>
2014-04-01 10:19:26 +0200

    replay-symbolic: s2e_print_expression API update

--- tests/trace/replay-symbolic.arm.c
+++ tests/trace/replay-symbolic.arm.c
@@ -1,7 +1,7 @@
 void _start() __attribute__((naked));
 
 extern void s2e_kill_state(int status, const char* message);
-extern void s2e_print_expression(int expr, int dummy, const char * msg);
+extern void s2e_print_expression(int expr, const char * msg);
 
 void _start() {
 
@@ -23,22 +23,22 @@ void _start() {
 
 	tmp = *a;
 	symb_tmp = tmp * 0x20;
-	s2e_print_expression(symb_tmp, 0, "first_var");
+	s2e_print_expression(symb_tmp, "first_var");
 	if (tmp != 0xdeadbeef)
 		s2e_kill_state(1, "ERROR: first read returned wrong result");
 
 	tmp = *b;
 	symb_tmp = tmp+0x20;
 	symb_tmp++;
-	s2e_print_expression(symb_tmp, 0, "second_var");
+	s2e_print_expression(symb_tmp, "second_var");
 	if (tmp != 0xbabe)
 		s2e_kill_state(2, "ERROR: second read returned wrong result");
 
 	tmp = *c;
 	symb_tmp = tmp+0x20;
-	s2e_print_expression(symb_tmp, 0, "third_var_not_zero");
+	s2e_print_expression(symb_tmp, "third_var_not_zero");
 	symb_tmp = 0x0;
-	s2e_print_expression(symb_tmp, 0, "third_var_zero");
+	s2e_print_expression(symb_tmp, "third_var_zero");
 	if (tmp != 0x13)
 		s2e_kill_state(3, "ERROR: third read returned wrong result");
 
